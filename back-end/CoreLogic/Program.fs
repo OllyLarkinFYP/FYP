4b15841eb66af765a0c870cc704f6e12e896e028
Olly Larkin <ollylemonlarkin@gmail.com>
2021-03-10 16:33:45 +0000

    PAR-11: Added tests for expressions

--- back-end/CoreLogic/Program.fs
+++ back-end/CoreLogic/Program.fs
@@ -4,8 +4,8 @@ open FParsec
 
 [<EntryPoint>]
 let main argv =
-    let testString = "test[123:1'd4]"
-    let parser = Expression.pExpression
+    let testString = "1 + 2"
+    let parser = ConstantExpression.pConstantExpression
     let result = run parser testString
     printfn "%A" result
     0 // return an integer exit code
