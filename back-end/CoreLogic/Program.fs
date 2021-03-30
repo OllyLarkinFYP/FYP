1bb8283b486bcc5b94faaa2dcb94cf821d42eaa7
Olly Larkin <ollylemonlarkin@gmail.com>
2021-03-30 13:28:55 +0100

    PAR-15: Just the program

--- back-end/CoreLogic/Program.fs
+++ back-end/CoreLogic/Program.fs
@@ -1,80 +1,131 @@
 open System
 open Parser
+open Parser.ConstantExpression
+open Parser.Expression
+open Parser.LangConstructs
+open Parser.Token
+open Parser.Utils
 open FParsec
 
 let program = @"
-module FIFO
-(
+module UART_TX(
   input wire clk,
   input wire resetn,
-  input wire rd,
-  input wire wr,
-  input wire [7:0] w_data,
+  input wire tx_start,        
+  input wire b_tick,          
+  input wire [8:0] d_in,      
+  output reg tx_done,         
+  output wire tx              
+  );
   
-  output wire empty,
-  output wire full,
-  output wire [7:0] r_data
-);
-
-//Internal Signal declarations
-
-  reg [7:0] array_reg;
-  reg [0:0] w_ptr_reg;
-  reg [0:0] w_ptr_next;
-  reg [0:0] w_ptr_succ;
-  reg [0:0] r_ptr_reg;
-  reg [0:0] r_ptr_next;
-  reg [0:0] r_ptr_succ;
-
-  reg full_reg;
-  reg empty_reg;
-  reg full_next;
-  reg empty_next;
-
-  wire w_en;
-
-  always @ (posedge clk)
-    if(w_en)
-    begin
-      array_reg[w_ptr_reg] <= w_data;
-    end
-
-  assign r_data = array_reg[r_ptr_reg];   
-
-  assign w_en = wr & ~full_reg;           
-
-  always @ (posedge clk, negedge resetn)
+  reg [1:0] current_state;
+  reg [1:0] next_state;
+  reg [3:0] b_reg;          
+  reg [3:0] b_next;
+  reg [3:0] count_reg;      
+  reg [3:0] count_next;
+  reg [8:0] data_reg;       
+  reg [8:0] data_next;
+  reg tx_reg;              
+  reg tx_next;
+  
+  always @(posedge clk, negedge resetn)
   begin
     if(!resetn)
       begin
-        w_ptr_reg <= 0;
-        r_ptr_reg <= 0;
-        full_reg <= 1'b0;
-        empty_reg <= 1'b1;
+        current_state <= idle_st;
+        b_reg <= 0;
+        count_reg <= 0;
+        data_reg <= 0;
+        tx_reg <= 1'b1;
       end
     else
       begin
-        w_ptr_reg <= w_ptr_next;
-        r_ptr_reg <= r_ptr_next;
-        full_reg <= full_next;
-        empty_reg <= empty_next;
+        current_state <= next_state;
+        b_reg <= b_next;
+        count_reg <= count_next;
+        data_reg <= data_next;
+        tx_reg <= tx_next;
       end
   end
 
 
-  //Next State Logic
   always @*
   begin
-    w_ptr_succ = w_ptr_reg + 1;
-    r_ptr_succ = r_ptr_reg + 1;
+    next_state = current_state;
+    tx_done = 1'b0;
+    b_next = b_reg;
+    count_next = count_reg;
+    data_next = data_reg;
+    tx_next = tx_reg;
     
-    w_ptr_next = w_ptr_reg;
-    r_ptr_next = r_ptr_reg;
-    full_next = full_reg;
-    empty_next = empty_reg;
+    case(current_state)
+      idle_st:
+      begin
+        tx_next = 1'b1;
+        if(tx_start)
+        begin
+          next_state = start_st;
+          b_next = 0;
+          data_next = d_in;
+        end
+      end
+      
+      start_st: 
+      begin
+        tx_next = 1'b0;
+        if(b_tick)
+          if(b_reg==15)
+            begin
+              next_state = data_st;
+              b_next = 0;
+              count_next = 0;
+            end
+          else
+            b_next = b_reg + 1;
+      end
+      
+      data_st: 
+      begin
+        tx_next = data_reg[0];
+        
+        if(b_tick)
+          if(b_reg == 15)
+            begin
+              b_next = 0;
+              data_next = data_reg >> 1;
+              if(count_reg == 8)    
+                next_state = stop_st;
+              else
+                count_next = count_reg + 1;
+            end
+          else
+            b_next = b_reg + 1;
+      end
+      
+      stop_st: 
+      begin
+        tx_next = 1'b1;
+        if(b_tick)
+          if(b_reg == 15)  
+            begin
+              next_state = idle_st;
+              tx_done = 1'b1;
+            end
+          else
+            b_next = b_reg + 1;
+      end
+    endcase
+  end
+  
+  assign tx = tx_reg;
+  
+endmodule
+"
+
+let testStr = @"
     
     case({w_en,rd})
-      //2'b00: nop
       2'b01:
         if(~empty_reg)
           begin
@@ -97,22 +148,13 @@ module FIFO
           r_ptr_next = r_ptr_succ;
         end
     endcase
-  end
-
-  //Set Full and Empty
-
-  assign full = full_reg;
-  assign empty = empty_reg;
-  
-endmodule
 "
 
 [<EntryPoint>]
 let main argv =
-    // let parser = LangConstructs.pSourceText false
-    // let result = run parser program
-    let testStr = "(input wire clk, input wire test)"
-    let parser = LangConstructs.pListOfPortDeclarations
-    let result = run parser testStr
+    let parser = pSourceText false
+    let result = run parser program
+    // let parser = spaces >>. Keyword.pCase >>. Symbol.pOpenRBrac >>. pExpression .>> Symbol.pCloseRBrac .>>. many1 (pCaseItem) .>> Keyword.pEndCase
+    // let result = run parser testStr
     printfn "%A" result
     0 // return an integer exit code
