372813a3b2e6a879df58848903e38d3fb28bebfa
Olly Larkin <ollylemonlarkin@gmail.com>
2021-05-31 17:01:34 +0100

    SIM-1: Added new program

--- back-end/CoreLogic/Program.fs
+++ back-end/CoreLogic/Program.fs
@@ -340,6 +340,100 @@ module FIFO
 endmodule
 "
 
+let alu = @"
+/* ALU Arithmetic and Logic Operations
+----------------------------------------------------------------------
+|ALU_Sel|   ALU Operation
+----------------------------------------------------------------------
+| 0000  |   ALU_Out = A + B;
+----------------------------------------------------------------------
+| 0001  |   ALU_Out = A - B;
+----------------------------------------------------------------------
+| 0010  |   ALU_Out = A * B;
+----------------------------------------------------------------------
+| 0011  |   ALU_Out = A / B;
+----------------------------------------------------------------------
+| 0100  |   ALU_Out = A << 1;
+----------------------------------------------------------------------
+| 0101  |   ALU_Out = A >> 1;
+----------------------------------------------------------------------
+| 0110  |   ALU_Out = A rotated left by 1;
+----------------------------------------------------------------------
+| 0111  |   ALU_Out = A rotated right by 1;
+----------------------------------------------------------------------
+| 1000  |   ALU_Out = A and B;
+----------------------------------------------------------------------
+| 1001  |   ALU_Out = A or B;
+----------------------------------------------------------------------
+| 1010  |   ALU_Out = A xor B;
+----------------------------------------------------------------------
+| 1011  |   ALU_Out = A nor B;
+----------------------------------------------------------------------
+| 1100  |   ALU_Out = A nand B;
+----------------------------------------------------------------------
+| 1101  |   ALU_Out = A xnor B;
+----------------------------------------------------------------------
+| 1110  |   ALU_Out = 1 if A>B else 0;
+----------------------------------------------------------------------
+| 1111  |   ALU_Out = 1 if A=B else 0;
+----------------------------------------------------------------------*/
+// fpga4student.com: FPGA projects, Verilog projects, VHDL projects
+// Verilog project: Verilog code for ALU
+// by FPGA4STUDENT
+module alu
+(
+    input [7:0] A,B,  // ALU 8-bit Inputs                 
+    input [3:0] ALU_Sel,// ALU Selection
+    output [7:0] ALU_Out, // ALU 8-bit Output
+    output CarryOut // Carry Out Flag
+);
+    reg [7:0] ALU_Result;
+    wire [8:0] tmp;
+
+    assign ALU_Out = ALU_Result; // ALU out
+    assign tmp = {1'b0,A} + {1'b0,B};
+    assign CarryOut = tmp[8]; // Carryout flag
+    
+    always @(*) begin
+        case(ALU_Sel)
+            4'b0000: // Addition
+                ALU_Result = A + B ; 
+            4'b0001: // Subtraction
+                ALU_Result = A - B ;
+            4'b0010: // Multiplication
+                ALU_Result = A * B;
+            4'b0011: // Division
+                ALU_Result = A/B;
+            4'b0100: // Logical shift left
+                ALU_Result = A<<1;
+            4'b0101: // Logical shift right
+                ALU_Result = A>>1;
+            4'b0110: // Rotate left
+                ALU_Result = {A[6:0],A[7]};
+            4'b0111: // Rotate right
+                ALU_Result = {A[0],A[7:1]};
+            4'b1000: //  Logical and 
+                ALU_Result = A & B;
+            4'b1001: //  Logical or
+                ALU_Result = A | B;
+            4'b1010: //  Logical xor 
+                ALU_Result = A ^ B;
+            4'b1011: //  Logical nor
+                ALU_Result = ~(A | B);
+            4'b1100: // Logical nand 
+                ALU_Result = ~(A & B);
+            4'b1101: // Logical xnor
+                ALU_Result = ~(A ^ B);
+            4'b1110: // Greater comparison
+                ALU_Result = (A>B)?8'd1:8'd0 ;
+            4'b1111: // Equal comparison   
+              ALU_Result = (A==B)?8'd1:8'd0 ;
+            default: ALU_Result = A + B ; 
+        endcase
+    end
+endmodule
+"
+
 let print str = printfn "%A" str
 
 [<EntryPoint>]
@@ -353,17 +447,15 @@ let main _ =
         printfn "[%s] elapsed (ms): %i" label stopwatch.ElapsedMilliseconds
         a
         
-    let modules = [fifo]
-    let topLevel = "FIFO"
+    let modules = [alu]
+    let topLevel = "alu"
     let inputs =
-        [ "clk", Repeating [VNum 0; VNum 1]
-          "resetn", Once [VNum 0; VNum 0; VNum 0; VNum 1]
-          "rd", Once []
-          "wr", Once []
-          "w_data", Once [] ]
+        [ "A", Once [ VNum 5 ]
+          "B", Once [ VNum 10 ]
+          "ALU_Sel", Once [ VNum 0; VNum 1; VNum 2; VNum 3; VNum 4; VNum 5; VNum 6; VNum 7; VNum 8; VNum 9; VNum 10; VNum 11; VNum 12; VNum 13; VNum 14; VNum 15 ] ]
         |> Map.ofList
-    let numberOfCycles = 10u
-    let reqVars = [ "clk"; "resetn"; "rd"; "wr"; "w_data"; "empty"; "full"; "r_data" ]
+    let numberOfCycles = 16u
+    let reqVars = [ "A"; "B"; "ALU_Sel"; "ALU_Out"; "CarryOut" ]
 
     let parser = LangConstructs.pSourceText
     modules
