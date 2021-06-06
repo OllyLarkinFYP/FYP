651872e25ebfc4c75d5860b99ffc01f46e4ac2ff
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-06 19:02:18 +0100

    Now using StdComm for sommunication

--- back-end/CoreLogic/Program.fs
+++ back-end/CoreLogic/Program.fs
@@ -1,20 +1,11 @@
 open System
 open MethodDispatcher
-
-let getJob () =
-    // TODO: get 4 bytes stating how long the message will be
-    // TODO: get n bytes of message
-    // TODO: potential issues: if they go out of sync from poorly constructed message?
-    Console.ReadLine()
-
-let postReply reply = 
-    // TODO: reply should be posed using same format as getJob():
-    // TODO: sned 4 bytes stating how long the message will be
-    // TODO: send n bytes of message
-    printfn "%s" reply
+open StdComm
 
 [<EntryPoint>]
 let main _ =
     printfn "%s" <| MethodDispatcher.GetSerializedExternalDeclaration()
-    MethodDispatcher(getJob, postReply).Start()
+    
+    let io = IO(Console.OpenStandardInput(), Console.OpenStandardOutput())
+    MethodDispatcher(io.receive, io.send).Start()
     0
