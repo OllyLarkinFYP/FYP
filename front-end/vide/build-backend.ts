dd4a3d50fa908fcc128b1d5f55d8470893e7fa50
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-07 16:25:06 +0100

    FE-1: Removed application removal from build script as it is small

--- front-end/vide/build-backend.ts
+++ front-end/vide/build-backend.ts
@@ -5,12 +5,9 @@ const BUILD_BACK_END =
     "cd ../../back-end/CoreLogic && dotnet publish -c Release";
 const FRONT_END_FOLDER = "resources/back-end";
 const BACK_END_FOLDER = "../../back-end/CoreLogic/bin/Release/net5.0";
-const EXE_PATH = "resources/back-end/CoreLogic.exe";
 
 fs.rmdirSync(FRONT_END_FOLDER, { recursive: true });
 
 execSync(BUILD_BACK_END);
 
 fs.copySync(BACK_END_FOLDER, FRONT_END_FOLDER);
-
-fs.unlinkSync(EXE_PATH);
