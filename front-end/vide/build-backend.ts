6682a8efebcdf92a4205f2701a08fd948a5f057d
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-07 14:55:45 +0100

    FE-1: Added backend build as part of build script and added to .gitignore

---
+++ front-end/vide/build-backend.ts
@@ -0,0 +1,16 @@
+import { exec, execSync } from "child_process";
+import * as fs from "fs-extra";
+
+const BUILD_BACK_END =
+    "cd ../../back-end/CoreLogic && dotnet publish -c Release";
+const FRONT_END_FOLDER = "resources/back-end";
+const BACK_END_FOLDER = "../../back-end/CoreLogic/bin/Release/net5.0";
+const EXE_PATH = "resources/back-end/CoreLogic.exe";
+
+execSync(BUILD_BACK_END);
+
+fs.rmdirSync(FRONT_END_FOLDER, { recursive: true });
+
+fs.copySync(BACK_END_FOLDER, FRONT_END_FOLDER);
+
+fs.unlinkSync(EXE_PATH);
