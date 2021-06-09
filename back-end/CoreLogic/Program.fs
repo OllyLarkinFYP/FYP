307eb17cb64d9d351cf5bb4986a785f2fc078acd
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-09 23:08:46 +0100

    FRA-15: Declaration is now printed to stdout when -d is provided

--- back-end/CoreLogic/Program.fs
+++ back-end/CoreLogic/Program.fs
@@ -14,22 +14,24 @@ let log (str: string) =
 
 [<EntryPoint>]
 let main argv =
-    if argv.Length > 0
-    then
-        let port = int argv.[0]
-        let ipAddress = IPAddress.Loopback
-        let endPoint = IPEndPoint(ipAddress, port)
+    if argv.Length = 1 && argv.[0] = "-c"
+    then printfn "%s" <| MethodDispatcher.GetSerializedExternalDeclaration()
+    else 
+        if argv.Length = 2 && argv.[0] = "-p"
+        then
+            let port = int argv.[1]
+            let ipAddress = IPAddress.Loopback
+            let endPoint = IPEndPoint(ipAddress, port)
 
-        let s = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp)
-        s.Bind(endPoint)
-        s.Listen()
-        log <| sprintf "Listening on port %i..." port
+            let s = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp)
+            s.Bind(endPoint)
+            s.Listen()
+            log <| sprintf "Listening on port %i..." port
 
-        let handler = s.Accept()
-        let stream = new NetworkStream(handler)
-        log "Connected."
+            let handler = s.Accept()
+            let stream = new NetworkStream(handler)
+            log "Connected."
 
-        let io = IO(stream, stream)
-        io.send(MethodDispatcher.GetSerializedExternalDeclaration())
-        MethodDispatcher(io.receive, io.send).Start()
+            let io = IO(stream, stream)
+            MethodDispatcher(io.receive, io.send).Start()
     0
