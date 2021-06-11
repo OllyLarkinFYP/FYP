951996f502c88b06e53dcb58d098694fb78d955b
Olly Larkin <ollylemonlarkin@gmail.com>
2021-06-11 12:25:04 +0100

    FE-NONE: Slightly neater

--- front-end/vide/src/execute-job.ts
+++ front-end/vide/src/execute-job.ts
@@ -20,50 +20,43 @@ export const executeJob = (
     replyCallBack?: (reply: any) => void
 ) => {
     console.log("Executing job:", job);
-    exec(
-        `dotnet ${backendPath} -j ${JSON.stringify(
-            JSON.stringify({ ...job, id: 0 })
-        )}`,
-        (err, stdout, stderr) => {
-            if (!err) {
-                if (stderr) {
+    const jobString = JSON.stringify(JSON.stringify({ ...job, id: 0 }));
+    exec(`dotnet ${backendPath} -j ${jobString}`, (err, stdout, stderr) => {
+        if (!err) {
+            if (stderr) {
+                ExtensionComponents.sendErrorToOutputChannel(
+                    "Backend produced errors while trying to process the request",
+                    `Backend produced errors while trying to process the request:\n${stderr}`
+                );
+            } else if (stdout) {
+                let response: IncomingReply = {};
+                try {
+                    response = JSON.parse(stdout);
+                } catch (jsonErr) {
                     ExtensionComponents.sendErrorToOutputChannel(
-                        "Backend produced errors while trying to process the request",
-                        `Backend produced errors while trying to process the request:\n${stderr}`
+                        "Backend did not return the result in a valid JSON format.",
+                        `Backend did not return the result in a valid JSON format. Returned string:\n${stdout}`
                     );
-                } else if (stdout) {
-                    let response: IncomingReply = {};
-                    try {
-                        response = JSON.parse(stdout);
-                    } catch (jsonErr) {
-                        ExtensionComponents.sendErrorToOutputChannel(
-                            "Backend did not return the result in a valid JSON format.",
-                            `Backend did not return the result in a valid JSON format. Returned string:\n${stdout}`
-                        );
-                    }
-                    if (
-                        response.id !== undefined &&
-                        response.reply !== undefined
-                    ) {
-                        console.log("Received reply:", response.reply);
-                        if (replyCallBack) {
-                            replyCallBack(response.reply);
-                        }
-                    } else {
-                        ExtensionComponents.sendErrorToOutputChannel(
-                            "Backend did not return the result in a valid format.",
-                            `Backend did not return the result in a valid format. Returned string:\n${stdout}`
-                        );
+                }
+                if (response.id !== undefined && response.reply !== undefined) {
+                    console.log("Received reply:", response.reply);
+                    if (replyCallBack) {
+                        replyCallBack(response.reply);
                     }
                 } else {
-                    console.error("Execution of job produced no output");
+                    ExtensionComponents.sendErrorToOutputChannel(
+                        "Backend did not return the result in a valid format.",
+                        `Backend did not return the result in a valid format. Returned string:\n${stdout}`
+                    );
                 }
             } else {
-                ExtensionComponents.sendErrorToOutputChannel(
-                    "Backend threw an exception while trying to process the request",
-                    `Backend threw an exception while trying to process the request:\n${err.name}:\n${err.message}`
-                );
+                console.error("Execution of job produced no output");
             }
+        } else {
+            ExtensionComponents.sendErrorToOutputChannel(
+                "Backend threw an exception while trying to process the request",
+                `Backend threw an exception while trying to process the request:\n${err.name}:\n${err.message}`
+            );
         }
-    );
+    });
 };
